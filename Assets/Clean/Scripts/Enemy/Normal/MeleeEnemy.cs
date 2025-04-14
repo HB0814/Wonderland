2d1f8e1f21d833da404801ab21c63ad0b38c5200
Potato-Gamja <90387670+Potato-Gamja@users.noreply.github.com>
2025-04-14 18:26:14 +0900

    Delete Assets/Clean/Scripts directory

--- Assets/Clean/Scripts/Enemy/Normal/MeleeEnemy.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class MeleeEnemy : Enemy
-{
-    protected override void Update()
-    {
-        base.Update();
-    }
-} 
--- Assets/Clean/Scripts/WeaponScripts/_WeaponData/WeaponDataHolder.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class WeaponDataHolder : MonoBehaviour
-{
-    public WeaponData weaponData;
-}
